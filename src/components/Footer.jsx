418435dfb6cbaaa4168cf336ac4f259f9816032c
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2025-02-17 02:18:59 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className=" bg-black text-white rounded-t-3xl mt-8 md:mt-0">
       <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
         <div className=" w-full md:w-1/4">
-          <h1 className=" font-semibold text-xl pb-4">FoodieWeb</h1>
+          <h1 className=" font-semibold text-xl pb-4">Faster-Food</h1>
           <p className=" text-sm">
           Delicie-se com uma sinfonia de sabores, onde cada prato é uma tela para
           excelência culinária.
